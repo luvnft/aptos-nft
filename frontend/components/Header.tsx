e22216d9c2ccd91f7ad01b892693204e95e325e8
ryorod <ryo3ishi28@gmail.com>
2024-08-20 10:20:32 +0900

    rm header's is dev

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -2,7 +2,6 @@ import { useGetCollectionData } from "@/hooks/useGetCollectionData";
 import { useMemo } from "react";
 import { Link } from "react-router-dom";
 import { WalletSelector } from "./WalletSelector";
-import { IS_DEV } from "@/constants";
 import { buttonVariants } from "@/components/ui/button";
 import { config } from "@/config";
 
@@ -20,16 +19,12 @@ export function Header() {
       </h1>
 
       <div className="flex gap-2 items-center flex-wrap">
-        {IS_DEV && (
-          <>
             <Link className={buttonVariants({ variant: "link" })} to={"/my-collections"}>
               My Collections
             </Link>
             <Link className={buttonVariants({ variant: "link" })} to={"/create-collection"}>
               Create Collection
             </Link>
-          </>
-        )}
         <WalletSelector />
       </div>
     </div>
