6931640e6692f1faa22775bbbde390f28e486aea
ryorod <ryo3ishi28@gmail.com>
2024-08-28 13:55:04 +0900

    new craft nft page

--- frontend/pages/CraftNFT.tsx
+++ frontend/pages/CraftNFT.tsx
@@ -2,9 +2,7 @@ import { LaunchpadHeader } from "@/components/LaunchpadHeader";
 import { aptosClient } from "@/utils/aptosClient";
 import { InputTransactionData, useWallet } from "@aptos-labs/wallet-adapter-react";
 import { useState } from "react";
-import { DndContext, DragEndEvent, DragOverlay, DragStartEvent, useDraggable, useDroppable } from "@dnd-kit/core";
-import { SortableContext, arrayMove, useSortable } from "@dnd-kit/sortable";
-import { CSS } from "@dnd-kit/utilities";
+import * as Dialog from "@radix-ui/react-dialog";
 
 interface NFT {
   id: string;
@@ -12,74 +10,49 @@ interface NFT {
   image: string;
 }
 
-const initialParentNFTs: NFT[] = [
-  { id: "1", name: "Parent NFT 1", image: "./assets/sword.png" },
-  { id: "2", name: "Parent NFT 2", image: "./assets/sword.png" },
-];
-
-const initialChildrenNFTsData: NFT[] = [
-  { id: "1", name: "Child NFT 1", image: "./assets/fire.png" },
-  { id: "2", name: "Child NFT 2", image: "./assets/fire.png" },
-  { id: "3", name: "Child NFT 3", image: "./assets/fire.png" },
+const allNFTs: NFT[] = [
+  { id: "1", name: "Sword", image: "./assets/sword-basic.png" },
+  { id: "2", name: "Fire", image: "./assets/element-fire.png" },
+  { id: "3", name: "Water", image: "./assets/element-water.png" },
 ];
 
 export function CraftNFT() {
   const { signAndSubmitTransaction } = useWallet();
-  const [selectedParent, setSelectedParent] = useState<NFT | null>(null);
-  const [selectedChildren, setSelectedChildren] = useState<NFT[]>([]);
-  const [initialChildrenNFTs, setInitialChildrenNFTs] = useState<NFT[]>(initialChildrenNFTsData);
-  const [activeId, setActiveId] = useState<string | null>(null);
+  const [selectedNFT1, setSelectedNFT1] = useState<NFT | null>(null);
+  const [selectedNFT2, setSelectedNFT2] = useState<NFT | null>(null);
+  const [selectedArea, setSelectedArea] = useState<string | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleDragStart = (event: DragStartEvent) => {
-    const { active } = event;
-    setActiveId(active.id.toString());
+  const handleAreaClick = (area: string) => {
+    setSelectedArea(area);
+    setIsModalOpen(true);
   };
 
-  const handleDragEnd = (event: DragEndEvent) => {
-    const { active, over } = event;
-    setActiveId(null);
-
-    if (over && over.id === "droppable-area" && active.id !== over.id) {
-      handleChildDrop(active.id.toString());
-    } else if (!over) {
-      handleUnselectChild(active.id.toString());
-    } else if (over && active.id !== over.id) {
-      setSelectedChildren((prev) => {
-        const oldIndex = prev.findIndex((nft) => nft.id === active.id);
-        const newIndex = prev.findIndex((nft) => nft.id === over.id);
-        return arrayMove(prev, oldIndex, newIndex);
-      });
+  const handleNFTSelect = (nft: NFT) => {
+    if (selectedArea === "area1") {
+      if (selectedNFT1?.id === nft.id) {
+        setSelectedNFT1(null);
+      } else {
+        setSelectedNFT1(nft);
+      }
+    } else if (selectedArea === "area2") {
+      if (selectedNFT2?.id === nft.id) {
+        setSelectedNFT2(null);
+      } else {
+        setSelectedNFT2(nft);
+      }
     }
-  };
-
-  const handleParentSelect = (nft: NFT) => {
-    setSelectedParent(nft);
-  };
-
-  const handleChildDrop = (id: string) => {
-    const droppedNFT = initialChildrenNFTs.find((nft) => nft.id === id);
-    if (droppedNFT && !selectedChildren.find((nft) => nft.id === droppedNFT.id)) {
-      setSelectedChildren((prev) => [...prev, droppedNFT]);
-      setInitialChildrenNFTs((prev) => prev.filter((nft) => nft.id !== id));
-    }
-  };
-
-  const handleUnselectChild = (id: string) => {
-    const removedNFT = selectedChildren.find((nft) => nft.id === id);
-    if (removedNFT) {
-      setSelectedChildren((prev) => prev.filter((nft) => nft.id !== id));
-      setInitialChildrenNFTs((prev) => [...prev, removedNFT]);
-    }
+    setIsModalOpen(false);
   };
 
   const handleSubmit = async () => {
-    if (!selectedParent || selectedChildren.length === 0) return;
+    if (!selectedNFT1 || !selectedNFT2) return;
 
     const payload: InputTransactionData = {
       data: {
         function: `${import.meta.env.VITE_MODULE_ADDRESS}::module::function`, // TODO: Add module and function names
         typeArguments: [],
-        functionArguments: [selectedParent.id, selectedChildren.map((nft) => nft.id)],
+        functionArguments: [selectedNFT1.id, selectedNFT2.id], // TODO: set correct arguments
       },
     };
 
@@ -96,136 +69,103 @@ export function CraftNFT() {
     <>
       <LaunchpadHeader title="Craft NFT" />
 
-      <div className="container mx-auto p-4">
-        <div className="flex">
-          <div className="w-1/4 p-4">
-            <h2 className="text-xl mb-2">Select Parent NFT</h2>
-            <div className="grid grid-cols-2 gap-2">
-              {initialParentNFTs.map((nft) => (
-                <button
-                  key={nft.id}
-                  className={`w-full h-32 border ${
-                    selectedParent?.id === nft.id ? "border-blue-500" : "border-gray-300"
-                  }`}
-                  onClick={() => handleParentSelect(nft)}
-                >
-                  <img src={nft.image} alt={nft.name} className="w-full h-full object-cover" />
-                </button>
-              ))}
-            </div>
+      <div className="container mx-auto p-4 pt-8">
+        <h2 className="text-2xl mb-8 text-center">Combine Your NFTs</h2>
+
+        <div className="flex justify-around items-start max-w-3xl mx-auto">
+          {/* Area 1 */}
+          <div className="w-1/3 p-4 border cursor-pointer" onClick={() => handleAreaClick("area1")}>
+            {selectedNFT1 ? (
+              <div>
+                <div>
+                  <img src={selectedNFT1.image} alt={selectedNFT1.name} className="w-full h-full object-cover" />
+                </div>
+                <p className="text-center pt-2">{selectedNFT1.name}</p>
+              </div>
+            ) : (
+              <p className="text-center">Click to select NFT 1</p>
+            )}
           </div>
 
-          <div className="w-3/4 flex justify-center items-start p-4">
-            {selectedParent && (
-              <div className="text-center">
-                <h2 className="text-xl">Selected Parent NFT</h2>
-                <div className="w-64 h-64 mx-auto border border-blue-500 mt-4">
-                  <img src={selectedParent.image} alt={selectedParent.name} className="w-full h-full object-cover" />
-                  <p className="mt-2">{selectedParent.name}</p>
+          {/* Area 2 */}
+          <div className="w-1/3 p-4 border cursor-pointer" onClick={() => handleAreaClick("area2")}>
+            {selectedNFT2 ? (
+              <div>
+                <div>
+                  <img src={selectedNFT2.image} alt={selectedNFT2.name} className="w-full h-full object-cover" />
                 </div>
+                <p className="text-center pt-2">{selectedNFT2.name}</p>
               </div>
+            ) : (
+              <p className="text-center">Click to select NFT 2</p>
             )}
           </div>
         </div>
 
-        {selectedParent && (
-          <div className="mt-10 text-center">
-            <h2 className="text-xl">Children NFTs</h2>
-            <DndContext onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
-              <DroppableArea id="droppable-area">
-                <SortableContext items={selectedChildren.map((nft) => nft.id)}>
-                  <div className="flex space-x-4 mt-4 justify-center">
-                    {selectedChildren.map((nft) => (
-                      <SortableNFT key={nft.id} nft={nft} />
-                    ))}
-                  </div>
-                </SortableContext>
-              </DroppableArea>
-              <div className="flex space-x-4 justify-center">
-                {initialChildrenNFTs.map((nft) => (
-                  <DraggableChildNFT key={nft.id} nft={nft} />
-                ))}
-              </div>
-              <DragOverlay>
-                {activeId ? (
-                  <div className="w-24 h-24 border border-green-500">
-                    <img
-                      src={
-                        initialChildrenNFTs.find((nft) => nft.id === activeId)?.image ||
-                        selectedChildren.find((nft) => nft.id === activeId)?.image
-                      }
-                      alt=""
-                      className="w-full h-full object-cover"
-                    />
-                    <p className="text-center">
-                      {initialChildrenNFTs.find((nft) => nft.id === activeId)?.name ||
-                        selectedChildren.find((nft) => nft.id === activeId)?.name}
-                    </p>
-                  </div>
-                ) : null}
-              </DragOverlay>
-            </DndContext>
-          </div>
-        )}
-
-        <div className="mt-20 text-center">
+        {/* Submit Button */}
+        <div className="text-center mt-12">
           <button
             className="px-4 py-2 bg-blue-500 text-white rounded"
             onClick={handleSubmit}
-            disabled={!selectedParent || selectedChildren.length === 0}
+            disabled={!selectedNFT1 || !selectedNFT2}
           >
-            Submit
+            Craft a new NFT
           </button>
         </div>
+
+        {/* Modal for selecting NFTs */}
+        <Dialog.Root open={isModalOpen} onOpenChange={setIsModalOpen}>
+          <Dialog.Portal>
+            <Dialog.Overlay className="fixed inset-0 bg-black opacity-80" />
+            <Dialog.Content
+              className="fixed bg-white p-6 shadow-lg rounded-lg"
+              style={{
+                maxWidth: "600px",
+                maxHeight: "80%",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                overflow: "auto",
+              }}
+            >
+              <Dialog.Title className="text-xl text-center mb-4">Select an NFT</Dialog.Title>
+              <div className="grid grid-cols-3 gap-4">
+                {allNFTs.map((nft) => {
+                  const isDisabled =
+                    (selectedArea === "area1" && nft.id === selectedNFT2?.id) ||
+                    (selectedArea === "area2" && nft.id === selectedNFT1?.id);
+                  const isSelectedInSameArea =
+                    (selectedArea === "area1" && nft.id === selectedNFT1?.id) ||
+                    (selectedArea === "area2" && nft.id === selectedNFT2?.id);
+
+                  return (
+                    <div
+                      key={nft.id}
+                      className={`${isDisabled ? "cursor-not-allowed" : "cursor-pointer"}`}
+                      onClick={() => !isDisabled && handleNFTSelect(nft)}
+                    >
+                      <div className={`relative p-2 border ${isSelectedInSameArea ? "border-2 border-green-400" : ""}`}>
+                        <img
+                          src={nft.image}
+                          alt={nft.name}
+                          className={`w-full h-full object-cover  ${isDisabled ? "opacity-50 grayscale-[50%]" : ""}`}
+                        />
+                        {isDisabled && <div className="absolute inset-0 bg-black opacity-80"></div>}
+                      </div>
+                      <p className={`text-center pt-2 ${isDisabled ? "opacity-50" : ""}`}>{nft.name}</p>
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="flex justify-end mt-6">
+                <Dialog.Close asChild>
+                  <button className="px-4 py-2 bg-gray-500 text-white rounded">Cancel</button>
+                </Dialog.Close>
+              </div>
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </div>
     </>
   );
 }
-
-const DraggableChildNFT: React.FC<{ nft: NFT }> = ({ nft }) => {
-  const { attributes, listeners, setNodeRef, transform } = useDraggable({
-    id: nft.id,
-  });
-
-  const style = {
-    transform: transform ? `translate3d(${transform.x}px, ${transform.y}px, 0)` : undefined,
-  };
-
-  return (
-    <div ref={setNodeRef} style={style} {...attributes} {...listeners} className="w-24 h-24 border border-gray-300">
-      <img src={nft.image} alt={nft.name} />
-      <p className="text-center">{nft.name}</p>
-    </div>
-  );
-};
-
-const DroppableArea: React.FC<{ id: string; children: React.ReactNode }> = ({ id, children }) => {
-  const { setNodeRef } = useDroppable({
-    id,
-  });
-
-  return (
-    <div ref={setNodeRef} className="border-2 border-dashed border-gray-500 p-4">
-      {children}
-    </div>
-  );
-};
-
-const SortableNFT: React.FC<{ nft: NFT }> = ({ nft }) => {
-  const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
-    id: nft.id,
-  });
-
-  const style = {
-    transform: CSS.Transform.toString(transform),
-    transition,
-    opacity: isDragging ? 0 : 1,
-  };
-
-  return (
-    <div ref={setNodeRef} style={style} {...attributes} {...listeners} className="w-24 h-24 border border-green-500">
-      <img src={nft.image} alt={nft.name} />
-      <p className="text-center">{nft.name}</p>
-    </div>
-  );
-};
