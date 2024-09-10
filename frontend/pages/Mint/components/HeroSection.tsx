04748d36aeec58cee42e88206f520305e51a47de
ryorod <ryo3ishi28@gmail.com>
2024-09-10 23:34:25 +0900

    update hero width

--- frontend/pages/Mint/components/HeroSection.tsx
+++ frontend/pages/Mint/components/HeroSection.tsx
@@ -5,10 +5,10 @@ interface HeroSectionProps {}
 export const HeroSection: React.FC<HeroSectionProps> = () => {
   return (
     <section>
-      <div className="mx-auto max-w-5xl">
+      <div className="mx-auto max-w-screen-xl xl:px-4 lg:w-3/4">
         <img src={HeroImage} alt="" />
       </div>
-      <p className="mx-auto max-w-5xl w-11/12 mt-8">
+      <p className="mx-auto max-w-screen-xl xl:px-4 lg:w-3/4 w-11/12 mt-8">
         Move NFT Studio is a user-friendly platform that simplifies the creation and management of NFTs on the Movement
         blockchain—no coding skills required. Our app empowers creators, collectors, and businesses by unlocking
         advanced NFT features, such as minting, combining, layering, composing, and more. Move NFT Studio is your
